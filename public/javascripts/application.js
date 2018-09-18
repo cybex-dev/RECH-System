67c464394955a5f2a5780742f839131ab4e167ff
KGCybeX <k4gcybex@gmail.com>
2018-09-18 09:02:32 +0200

    WIP GUI changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,8 +7,10 @@ function get(k) {
 _docReady(docReady());
 
 function docReady() {
-    fixTables();
-    addSearchPerson();
+    if (document.getElementById("application_type") != null) {
+        fixTables();
+        addSearchPerson();
+    }
 }
 
 function addSearchPerson() {
@@ -24,9 +26,9 @@ function fixTables() {
 
     //Get all table rows, the element and index
     document.querySelectorAll("table > thead > tr").forEach(row => {
-        var mockupRow = [];
+        const mockupRow = [];
         row.querySelectorAll("th").forEach(headerRow => {
-            var label;
+            let label;
             headerRow.childNodes.forEach(value => {
                 if (value.tagName === "BR"){
                     value.parentNode.removeChild(value);
@@ -34,13 +36,21 @@ function fixTables() {
                     label =  value;
                 }
             });
-            headerRow.classList.add("col-sm-5");
+            headerRow.classList.add("col-sm-4");
             let field = headerRow.children[2];
             headerRow.removeChild(field);
             mockupRow[mockupRow.length++] = field;
         });
         let table = row.parentElement.parentElement;
         tableElementsContainer[table.id] = mockupRow;
+
+        let th = document.createElement("TH");
+        th.innerText = "";
+        th.classList.add("col-sm-1");
+        let buttonContainer = th.cloneNode(true);
+        row.insertBefore(th, row.firstElementChild);
+        buttonContainer.classList.add("col-sm-2");
+        row.appendChild(buttonContainer);
 
         const tableBody = document.createElement("TBODY");
         const tableFooter = document.createElement("TFOOT");
@@ -59,37 +69,41 @@ function addAddEntryButtons() {
 
 
 function addEntryButton(id) {
-    var tableRef = document.getElementById(id).getElementsByTagName('tfoot')[0];
-    var button = document.createElement("BUTTON");
-    var footer = tableRef.insertRow(0);
-    var cell = footer.insertCell(0);
-    button.classList.add("nmu-button", "button-small");
+    const tableRef = document.getElementById(id).getElementsByTagName('tfoot')[0];
+    const button = document.createElement("BUTTON");
+    const footer = tableRef.insertRow(0);
+    const cell = footer.insertCell(0);
+    cell.colSpan = 2;
+    button.classList.add("nmu-button", "button-small", "col-lg-6");
     button.id = id + "_addrow";
     button.innerText = "Add";
-    button.onclick = function () {
+    button.onclick = function (event) {
         addRow(id, get(id));
+        event.preventDefault();
     };
     cell.appendChild(button)
 }
 
 function addRow(tableId, elements) {
-    var tableRef = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+    const tableRef = document.getElementById(tableId).getElementsByTagName('tbody')[0];
 
 // Insert a row in the table at the last row
-    var newRow   = tableRef.insertRow(tableRef.rows.length);
+    const newRow = tableRef.insertRow(tableRef.rows.length);
+    let cell = newRow.insertCell(0);
+    cell.innerText = tableRef.rows.length;
+    cell.style.color = "#0A283F";
+    cell.style.padding = "2px";
 
 // Insert a cell in the row at index 0
-    for (let i = 0; i < elements.size; i++) {
-        var newCell  = newRow.insertCell(i);
-        var e = elements[i];
-        e.id += "_" + i;
-        e.name += "_" + i;
+    for (let i = 0; i < elements.length; i++) {
+        const newCell = newRow.insertCell(i+1);
+        const e = elements[i].cloneNode(true);
+        e.id += "_" + tableRef.rows.length;
+        e.name += "_" + tableRef.rows.length;
+        e.classList.add("col-sm-1");
+        if (e.tagName === "SELECT") {
+            e.style.width = "auto";
+        }
         newCell.appendChild(e);
     }
-}
-
-
-
-function countRows(tableId) {
-    return $('#' + tableId + ' tr').length();
 }
